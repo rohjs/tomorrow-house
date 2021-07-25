03b65fad1faa3b7d22beab9be54a7c58aa4f7f42
Roh Woohyeon <roh.woohyeon@gmail.com>
2021-07-25 13:14:53 +0900

    🚧 Initiate a new carousel: .user-gallery

--- js/carousels.js
+++ js/carousels.js
@@ -10,3 +10,21 @@ const productCarousel = tns({
   mouseDrag: true,
   preventScrollOnTouch: true,
 })
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  gutter: 4,
+  edgePadding: 16,
+  controls: false,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      edgePadding: 75,
+      controls: true,
+    },
+  },
+})
