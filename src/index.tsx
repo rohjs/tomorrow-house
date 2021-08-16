b7d0d456ad058797c20c1f6d411f1ee51930a1ed
Roh Woohyeon <roh.woohyeon@gmail.com>
2021-08-17 00:17:23 +0900

    📦  Install react-router-dom and set router

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { store } from './app/store'
 import { theme } from './styles'
 
-import { GlobalStyle } from './globalStyle'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Route exact path="/" component={App} />
+        </ThemeProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
