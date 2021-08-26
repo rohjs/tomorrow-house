3585f2882c6f52d87e00923a70f2560bd6ed0447
Roh Woohyeon <roh.woohyeon@gmail.com>
2021-08-27 07:52:58 +0900

    🚧  Pass optional className to tag

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -5,13 +5,18 @@ import { StyledTag } from './styles'
 export type TagColor = 'red' | 'gray' | 'green'
 export interface TagProps {
   label: string
+  className?: string
   color?: TagColor
 }
 
-const Tag: React.FC<TagProps> = ({ label, color }) => {
+const Tag: React.FC<TagProps> = ({ label, className, color }) => {
   if (!label) return null
 
-  return <StyledTag color={color}>{label}</StyledTag>
+  return (
+    <StyledTag className={className} color={color}>
+      {label}
+    </StyledTag>
+  )
 }
 
 const MemoTag = memo(Tag)
