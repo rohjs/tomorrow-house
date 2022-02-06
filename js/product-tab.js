d6ba419fb59560275de6ea09b429660559e3931d
Roh Woohyeon <roh.woohyeon@gmail.com>
2022-02-07 00:10:16 +0900

    ⚡️ Improve performance by wrapping event handlers with lodash throttle

--- js/product-tab.js
+++ js/product-tab.js
@@ -106,5 +106,5 @@ function updateActiveTabOnScroll() {
 }
 
 window.addEventListener('load', detectTabPanelPosition)
-window.addEventListener('resize', detectTabPanelPosition)
-window.addEventListener('scroll', updateActiveTabOnScroll)
+window.addEventListener('resize', _.throttle(detectTabPanelPosition, 1000))
+window.addEventListener('scroll', _.throttle(updateActiveTabOnScroll, 300))
