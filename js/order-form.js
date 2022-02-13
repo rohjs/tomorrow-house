2e4190ef62a2a2df9be47f14ee8974a6f49166d9
Roh Woohyeon <roh.woohyeon@gmail.com>
2022-02-13 17:49:29 +0900

    Updates

--- js/order-form.js
+++ js/order-form.js
@@ -1,22 +1,16 @@
 const orderForm = document.querySelector('.floating-order-form')
 const bookmarkButton = orderForm.querySelector('.bookmark-button')
-
-const cartButton = orderForm.querySelector('.button-group .btn-outlined')
-const cartModal = document.querySelector('.cart-modal')
-const cartModalOverlay = document.querySelector('.overlay')
-const cartCloseButton = cartModal.querySelector('.btn-secondary')
-
 const globalFooter = document.querySelector('.global-footer')
 
 const TOP_HEADER_HEIGHT = 80 + 50 + 54
 const ignoreScrollEvent = 10000
 
-function adjustOrderFormHeightOnScroll() {
+function adjustOrderFormHeightOnScroll(e) {
   if (window.innerWidth < 1200) {
     return
   }
 
-  if (window.scrollY < ignoreScrollEvent) {
+  if (e.type === 'scroll' && window.scrollY < ignoreScrollEvent) {
     return
   }
 
@@ -32,6 +26,7 @@ function adjustOrderFormHeightOnScroll() {
   orderForm.style.height = `${formHeight}px`
 }
 
+window.addEventListener('load', adjustOrderFormHeightOnScroll)
 window.addEventListener(
   'scroll',
   _.throttle(adjustOrderFormHeightOnScroll, 100)
@@ -46,18 +41,3 @@ function toggleBookmarkButton() {
 }
 
 bookmarkButton.addEventListener('click', toggleBookmarkButton)
-
-function openCartModal() {
-  cartModal.classList.add('is-active')
-  cartModalOverlay.classList.add('is-active')
-}
-
-cartButton.addEventListener('click', openCartModal)
-
-function closeCartModal() {
-  cartModal.classList.remove('is-active')
-  cartModalOverlay.classList.remove('is-active')
-}
-
-cartModalOverlay.addEventListener('click', closeCartModal)
-cartCloseButton.addEventListener('click', closeCartModal)
